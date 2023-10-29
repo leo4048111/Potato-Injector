a933bad22fbc688fa91c0bca63c8a89ee2b31e05
Z3r0M3m0ry <liqitong@163.com>
2023-10-29 13:31:22 +0800

    Updated x64 configuration, removed win32
    Updated x64 configuration, removed win32

--- vars/vars.hpp
+++ vars/vars.hpp
@@ -4,7 +4,7 @@
 namespace vars
 {
 	inline std::wstring_view str_steam_process_name{ L"steam.exe" };
-	inline std::wstring_view str_game_process_name{ L"csgo.exe" };
+	inline std::wstring_view str_game_process_name{ L"cs2.exe" };
 	inline std::wstring_view str_dll_name{ L"cheat.dll" };
 	inline std::wstring_view str_steam_mod_name{ L"tier0_s.dll" };
 	inline std::wstring_view str_game_mod_name{ L"serverbrowser.dll" };
